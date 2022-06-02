7fbeda49e7544baa69ae28fb41fd542366c148cf
Jommel Dalisaymo <dalisaymo36@gmail.com>
2022-06-02 11:50:12 +0800

    Initial Commit
    Created a website named Callee's Kitchen - It is only a front-end website.

---
+++ script/script.js
@@ -0,0 +1,28 @@
+jQuery( document ).ready(function() {
+    var mywindow = jQuery(window);
+    updateStyling(mywindow.scrollTop());
+    console.log(mywindow.scrollTop());
+
+    var mywindow = jQuery(window);
+    var mypos = mywindow.scrollTop();
+    let scrolling = false;
+    window.addEventListener('scroll', function() {
+        console.log('scrolling');
+        mypos = mywindow.scrollTop();
+        scrolling = true;
+    });
+    setInterval(() => {
+        if (scrolling) {
+            scrolling = false;
+            updateStyling(mypos);
+        }
+    }, 200);
+
+    function updateStyling(mypos){
+        if (mypos >= 20) {
+            jQuery('#header-navbar').addClass('header-navscroll');
+        } else {
+            jQuery('#header-navbar').removeClass('header-navscroll');
+        }
+    }
+});
